0d32bae3bbbe721251605886c6ce436a65bf9852
SarswatiPatil <dipakalagate1991@gmail.com>
2019-12-14 14:55:34 +0530

    Added test for IPLMostRuns File when file type is incorrect should throw exception

--- src/test/java/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyserTest.java
@@ -9,6 +9,8 @@ public class IPLAnalyserTest
     private static final String IPL_FILE_PATH="/home/admin1/Desktop/IPL2019Analyser/src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String IPL_EMPTY_FILE_PATH="/home/admin1/Desktop/IPL2019Analyser/src/test/resources/IPL2019MostRuns.csv";
     private static final String IPL_WRONG_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_IPL_FILE_TYPE="/home/admin1/Desktop/CensusAnalyser/src/test/resources/IPL2019FactsheetMostRuns.csg";
+
     IPLAnalyser iplAnalyser=new IPLAnalyser();
 
     @Test
@@ -73,6 +75,21 @@ public class IPLAnalyserTest
         catch (CSVBuilderException e)
         {
             Assert.assertEquals(CSVBuilderException.ExceptionType.IPL_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunsData_IfFileTypeIncorrect_ShouldThrowException()
+    {
+        try
+        {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CSVBuilderException.class);
+            iplAnalyser.loadIPLAnalserData(WRONG_IPL_FILE_TYPE);
+        }
+        catch (CSVBuilderException e)
+        {
+            Assert.assertEquals(CSVBuilderException.ExceptionType.IPL_FILE_PROBLEM,e.type);
         }
     }
 }
